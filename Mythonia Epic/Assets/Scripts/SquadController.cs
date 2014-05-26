1ba38affe0d29aff7bc8bd1eba00fd012892b394
Cody Beaner <decamonos@gmail.com>
2014-05-25 22:27:01 -0400

    Selection works now!
    Found it was an issue with the Ray updating, not sure how it worked
    before.

--- Mythonia Epic/Assets/Scripts/SquadController.cs
+++ Mythonia Epic/Assets/Scripts/SquadController.cs
@@ -22,43 +22,53 @@ public class SquadController : MonoBehaviour {
 	private Transform currentTarget;
 	
 	void Start () {
-		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
 	}
 	
 	void Update () {
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-		Target();
-		if(selected == false && Input.GetMouseButton(0) && Target().parent.FindChild("Squad") == transform){
-			selected = true;
+		if(selected == false && Input.GetMouseButton(0)){
+			if(Physics.Raycast(ray, out hit, Mathf.Infinity)){
+				currentTarget = hit.transform;
+			}
+			if (currentTarget.tag == "Unit"){
+				if (currentTarget.parent.FindChild("Squad") == transform){
+					selected = true;}}
 		}
-		Target();
-		if(selected == true && Input.GetMouseButton(1) && Target().parent.FindChild("Squad") == transform){
-			selected = false;
+		if(selected == true && Input.GetMouseButton(1)){
+			if(Physics.Raycast(ray, out hit, Mathf.Infinity)){
+				currentTarget = hit.transform;
+			}
+			if (currentTarget.tag == "Unit"){
+				if (currentTarget.parent.FindChild("Squad") == transform){
+					selected = false;}}
 		}
-
+		
 		if(menu != null && Input.GetMouseButtonUp(0)){
 			Destroy(menu);
 		}
 	
-		if(Input.GetMouseButtonDown(0) && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity)){
-			if(hit.transform.parent == transform.parent){
+		//if(Input.GetMouseButtonDown(0) && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity)){
+			//if(hit.transform.parent == transform.parent){
 				//selected = true;
-			}
+			//}
 		
-		if(selected && hit.transform.tag == "Ground"){
+		if(selected && Input.GetMouseButtonDown(0)){
+			if(Physics.Raycast(ray, out hit, Mathf.Infinity)){
+				currentTarget = hit.transform;
+			}
+			if (hit.transform.tag == "Ground"){
 			transform.position = hit.point + new Vector3(0, 0.1f, 0);
 			menu = Instantiate(menuPrefab, hit.point + (hit.normal * 0.01f), Quaternion.LookRotation(hit.normal + new Vector3(-90,0,0))) as GameObject;
-			}
+			}}
 		}
 		
-	}
 
-	public Transform Target(){
-		if(Physics.Raycast(ray, out hit, Mathf.Infinity)){
-			return hit.transform;
-		}
+
+	 void Target(){
+
 		// This is an escape to return the current target if no raycast can be found, without this an error occurs.
-		return currentTarget;
 	}
 
 }
