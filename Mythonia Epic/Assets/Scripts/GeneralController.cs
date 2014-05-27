33308efd15c7dd9af7eca8682c81f5782df24fe0
Cody Beaner <decamonos@gmail.com>
2014-05-27 05:17:55 -0400

    Added General starting stuff
    Using menu_move while clicking the general will now cause skill 0 to be
    selected and can be used by clicking elsewhere

--- Mythonia Epic/Assets/Scripts/GeneralController.cs
+++ Mythonia Epic/Assets/Scripts/GeneralController.cs
@@ -3,13 +3,56 @@ using System.Collections;
 
 public class GeneralController : MonoBehaviour {
 
+	private GameObject _general;
+
+	private RaycastHit hit;
+
+	public GameObject skillZeroObject;
+
+	public GameObject menuPrefab;
+	private GameObject menu;
+
+	public bool usingSkill = false;
+	private int skillUse;
+
 	// Use this for initialization
 	void Start () {
-	
+		_general = gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity);
+		if (Input.GetMouseButtonDown(0) && hit.transform != null){
+
+				if(hit.transform.tag == "General"){
+					menu = Instantiate(menuPrefab, hit.point + (hit.normal * 0.01f), Quaternion.LookRotation(hit.normal + new Vector3(-90,0,0))) as GameObject;
+				}
+
+				if (usingSkill == true){
+				Skill(skillUse);
+				usingSkill = false;
+				}
+			}
+
+		if(Input.GetMouseButtonUp(0)){
+			if(hit.transform != null){
+				// add in different commands here
+			if(hit.transform.name == "menu_move"){
+					usingSkill = true;
+					skillUse = 0;
+				}}
+			
+				Destroy(menu);
+			}
+
 	}
+	void Skill(int skillNumber){
+		if (skillNumber == 0){
+			Instantiate(skillZeroObject, hit.point + new Vector3(0,10,0), Quaternion.identity);
+		}
+	}
+
+
+
 }
